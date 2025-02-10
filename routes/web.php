cf0b7d407895b950c135b3a0a4e3d594d6d1735e
ahmedbenseddiq <benseddiqahmed4@gmail.com>
2025-02-10 15:23:22 +0100

    category routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/category', [CategoryController::class, 'index']);
+Route::get('/category/create', [CategoryController::class,'create']);
+Route::post('/category', [CategoryController::class,'store']);
+Route::get('/catgeory/{id}', [CategoryController::class, 'show']);
+Route::get('category/{id}/edit', [CategoryController::class,'edit']);
+Route::put('/category/{id}', [CategoryController::class,'update']);
+Route::delete('/category/{id}', [CategoryController::class,'delete']);
+
+
